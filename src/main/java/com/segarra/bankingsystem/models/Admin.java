8bd3463dcb37e4ebc540c829b68304b10684b9e3
Ana Segarra <anasegarraber@gmail.com>
2020-06-22 11:49:17 +0200

    Add user models

--- src/main/java/com/segarra/bankingsystem/models/Admin.java
+++ src/main/java/com/segarra/bankingsystem/models/Admin.java
@@ -1,4 +1,35 @@
 package com.segarra.bankingsystem.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "admins")
 public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    public Admin() {
+    }
+
+    public Admin(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
